323d1ad29ecb793aea05fd08bc5c53c3723bdd1c
Janne Mattila <janne.mattila@hotmail.fi>
2022-05-27 11:19:21 +0300

    Add touch move support

--- src/StickyNotes/wwwroot/js/site.js
+++ src/StickyNotes/wwwroot/js/site.js
@@ -1,5 +1,6 @@
 ﻿let notesElement = document.getElementById("notes");
 
+let isMove = false;
 let isResize = false;
 let currentX, currentY, endX, endY;
 let sourceElement = undefined;
@@ -14,33 +15,43 @@ const deselectNotes = () => {
 }
 
 const pointerDown = e => {
-    currentX = e.clientX || e.changedTouches[0].pageX;
-    currentY = e.clientY || e.changedTouches[0].pageY;
+    currentX = e.clientX;
+    currentY = e.clientY;
 
     const width = e.srcElement.offsetWidth;
     const height = e.srcElement.offsetHeight;
-    const offsetX = e.offsetX || e.changedTouches[0].screenX - e.changedTouches[0].clientX;
-    const offsetY = e.offsetY || e.changedTouches[0].screenY - e.changedTouches[0].clientY;
 
     deselectNotes();
     selectedElement = sourceElement = e.srcElement;
     sourceElement.className += " selected";
-    isResize = offsetX >= width * 0.7 && offsetY >= height * 0.7;
-    e.preventDefault();
+    isResize = e.offsetX >= width * 0.7 && e.offsetY >= height * 0.7;
+    e.stopPropagation();
 }
 
 const pointerMove = e => {
+    e.stopPropagation();
+
     if (sourceElement === undefined) {
+        if (isMove) {
+            endX = currentX - e.clientX;
+            endY = currentY - e.clientY;
+            currentX = e.clientX;;
+            currentY = e.clientY;
+
+            const notes = document.getElementsByClassName("stickynote");
+            for (let i = 0; i < notes.length; i++) {
+                const element = notes[i];
+                element.style.top = `${element.offsetTop - endY}px`;
+                element.style.left = `${element.offsetLeft - endX}px`;
+            }
+        }
         return;
     }
 
-    const currentClientX = e.clientX || e.changedTouches[0].pageX;
-    const currentClientY = e.clientY || e.changedTouches[0].pageY;
-
-    endX = currentX - currentClientX;
-    endY = currentY - currentClientY;
-    currentX = currentClientX;
-    currentY = currentClientY;
+    endX = currentX - e.clientX;
+    endY = currentY - e.clientY;
+    currentX = e.clientX;;
+    currentY = e.clientY;
 
     if (isResize) {
         const width = Math.floor(sourceElement.style.width.replace("px", ""));
@@ -56,13 +67,13 @@ const pointerMove = e => {
 }
 
 const pointerUp = e => {
+    isMove = false;
     sourceElement = undefined;
-    e.preventDefault();
     e.stopPropagation();
 }
 
-document.addEventListener("pointermove", pointerMove);
-document.addEventListener("pointerup", pointerUp);
+window.addEventListener("pointermove", pointerMove, { passive: true });
+window.addEventListener("pointerup", pointerUp);
 
 let protocol = new signalR.JsonHubProtocol();
 let hubRoute = "Notes";
@@ -79,7 +90,7 @@ const addNote = (note) => {
     element.style.width = "100px";
     element.style.height = "100px";
     element.style.transform = `rotateZ(${Math.floor(Math.random() * 8) - 4}deg)`;
-    element.addEventListener("pointerdown", pointerDown);
+    element.addEventListener("pointerdown", pointerDown, { passive: true });
     element.addEventListener("dblclick", e => {
         let note = prompt("Add note", element.innerText);
         if (note === undefined || note == null || note.length === 0) {
@@ -96,6 +107,7 @@ const addNote = (note) => {
 }
 
 const showNoteDialog = () => {
+    isMove = false;
 
     while (true) {
         let note = prompt("Add note");
@@ -112,9 +124,12 @@ window.addEventListener('focus', () => {
 window.addEventListener('blur', () => {
 });
 
-window.addEventListener('mouseup', e => {
-    console.log("window mouseup");
+window.addEventListener('pointerdown', e => {
     deselectNotes();
+
+    currentX = e.clientX;
+    currentY = e.clientY;
+    isMove = true;
 });
 
 window.addEventListener('contextmenu', e => {
@@ -161,7 +176,7 @@ function hideHelp() {
 }
 
 document.addEventListener('keyup', (e) => {
-    if (e.key === "Escape") {
+    if (e.key === "Escape" || e.key === "Alt" || e.key === "F12" || e.key === "Tab") {
     }
     else if (e.key === "Backspace" || e.key === "Delete") {
         if (selectedElement !== undefined) {
@@ -169,9 +184,8 @@ document.addEventListener('keyup', (e) => {
             selectedElement = undefined;
         }
     }
-    else if (e.key === "Alt") {
-    }
     else if (!e.altKey) {
+        console.log(e);
         showNoteDialog();
     }
 });
